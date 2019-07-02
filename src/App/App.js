ad0b4fa972c76de9263e3ef646afa9e099fe9a2e
Calvin J Foster <cal.j.foster@gmail.com>
2019-07-02 13:39:00 -0500

    completed authentication

--- src/App/App.js
+++ src/App/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -2,23 +2,22 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-import './Auth.scss';
-
 class Auth extends React.Component {
-    loginClickEvent = (e) => {
-      e.preventDefault();
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider);
-    };
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  };
 
-    render() {
-      return (
-            <div className="Auth">
-                <h1>Auth</h1>
-                <button className="btn btn-secondary" onClick={this.loginClickEvent}>Login with Google</button>
-            </div>
-      );
-    }
+  render() {
+    return (
+      <div className="Auth">
+        <h1 className="loginAuth">Scat Surprise</h1>
+        <h5 className="loginAuth">"What kinda CRAP is this"</h5>
+        <button className="btn btn-secondary" onClick={this.loginClickEvent}>Log In!</button>
+      </div>
+    );
+  }
 }
 
 export default Auth;
