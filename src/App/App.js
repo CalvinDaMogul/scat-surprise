ced0b9aa8cea09f9d3f7c90afee44be60b677d05
Calvin J Foster <cal.j.foster@gmail.com>
2019-07-02 00:51:39 -0500

    auth is halfway setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,54 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
-function App() {
+import Home from '../components/Home/Home';
+import Auth from '../components/Auth/Auth';
+import MyNavBar from '../components/MyNavbar/MyNavBar';
+
+import fbConnection from '../helpers/data/connections';
+
+fbConnection();
+
+
+// import './App.scss';
+
+class App extends React.Component {
+state= {
+  authed: false,
+}
+
+componentDidMount() {
+  this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ authed: true });
+    } else {
+      this.setState({ authed: false });
+    }
+  });
+}
+
+componentWillUnmount() {
+  this.removeListener();
+}
+
+render() {
+  const { authed } = this.state;
+  const loadComponent = () => {
+    if (authed) {
+      return (
+        <Home />
+      );
+    }
+    return <Auth />;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <MyNavBar authed={authed} />
+    {loadComponent()}
+  </div>
   );
+}
 }
 
 export default App;
