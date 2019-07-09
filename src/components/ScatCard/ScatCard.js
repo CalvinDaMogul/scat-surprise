a21c268b41db7aafa002c99822de993ce50bafdf
CalvinDaMogul <cal.j.foster@gmail.com>
2019-07-08 23:45:44 -0500

    ultiple changes made

---
+++ src/components/ScatCard/ScatCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import scatShape from '../../helpers/propz/scatShape';
+
+
+class ScatCard extends React.Component {
+  static propTypes = {
+    scat: scatShape.scatCardShape,
+    deleteScat: PropTypes.func.isRequired,
+  }
+
+  deleteMe = (e) => {
+    e.preventDefault();
+    const { scat, deleteScat } = this.props;
+    deleteScat(scat.id);
+  }
+
+  render() {
+    const { scat } = this.props;
+    const singleLink = `/scat/${scat.id}`;
+    return (
+      <div className="ScatCard col-4">
+        <div className="card">
+          <div className="card-body">
+          <h5 className="card-title">{scat.sampleName}</h5>
+          <Link className="btn btn-success" to={singleLink}>View</Link>
+          <p className="card-text">{scat.location}</p>
+          <button className="btn btn-danger" onClick={this.deleteMe}>Delete</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ScatCard;
